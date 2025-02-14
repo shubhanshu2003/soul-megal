6f6e516b139343a1e9a2868413d168e1faa7e36b
shubhanshu2003 <chaubeyshubhanshu30@gmail.com>
2025-02-14 11:15:29 +0530

    port changes

--- soulmegal/vite.config.js
+++ soulmegal/vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 10000, // Use PORT env variable or fallback to 5173
+  }
 })
